e6416680e0066a83f394eca032e3b894255a9cdd
Dean Shaff <dshaff001@gmail.com>
2017-11-19 13:17:38 +0400

    Didn't make much progress. Need to sort out end goal of project

--- src/app.js
+++ src/app.js
@@ -8,26 +8,26 @@ function App(){
         var self = this ;
         $("#word-list").append(function(){
             var scrollable = self.generateWordListHTML(ar_to_en);
-            console.log(scrollable.join(""));
             var div = `
             <div class="scrollable-box-container">
                 <div class="scrollable-box">
-                <div class="bottom-spacing"></div>
+                <div id="word-list-contents">
                 ${scrollable.join("")}
+                </div>
             </div>
             </div>`;
             return $(div)
                     .css("max-height", $(window).height())
                     .css("width",$("#word-list").width());
         })
-        // ("#word-list").on("click", header, this.wordListClickCb(this));
+        $("#word-list-contents").on("click",".word-list-entry", this.wordListClickCb(this));
         $("#search-area").append(function(){
             var div = `
             <div>
                 <label for="search-input">Search in Arabic or English</label>
                 <input type="search" placeholder="type word here..." id="search-input">
             </div>
-            <div></div>
+            <div id="word-decription"></div>
             `
             return $(div);
         })
@@ -36,25 +36,38 @@ function App(){
         $(window).on("resize", this.resizeCb(this))
     }
 
+    this.wordListClickCb = function(self){
+        return function(){
+            var wordText = $(this).find("h5").html().split(" | ");
+            var en_word = wordText[0];
+            var ar_word = wordText[1];
+            $("#word-decription").html(function(){
+                var html = "";
+                var arabic = en_to_ar[en_word]['ar'];
+                html += `<h5>${en_word}</h5>`;
+                var fos7a = arabic['fos7a'];
+                html += `<h5> فصحى : ${fos7a}</h5>`
+                if ("3mia" in arabic){
+                    html += `<h5>عامية : ${arabic['3mia']}</h5>`;
+                }
+                return $(html);
+            })
+        }
+    }
+
     this.searchKeyDownCb = function(self){
         return function(event){
-            var currentVal = $(this).val();
+            var currentVal = $(this).val().toLowerCase();
+            var scrollable ;
             if (currentVal == ""){
-                $("#search-area div").next().html("");
+                scrollable = self.generateWordListHTML(ar_to_en);
+                console.log(scrollable);
+                $("#word-list-contents").html(`${scrollable.join("")}`);
                 return;
             }
             var possibleMatches = self.searchDictionary(currentVal);
-            var htmlMatches = [];
-            var header = "h5"
-            possibleMatches.forEach(function(e){
-                htmlMatches.push(`<${header}>${Object.values(e).join(", ")}</${header}>`)
-            })
-            $("#search-area div").next().html(function(){
-                var div = `
-                ${htmlMatches.join("")}
-                `
-                return $(div)
-            })
+            scrollable = self.generateWordListHTML(possibleMatches);
+            $("#word-list-contents").html(`${scrollable.join("")}`);
         }
     }
 
@@ -65,12 +78,24 @@ function App(){
         }
     }
 
+
+    /**
+     * @function generateWordListHTML
+     * @param  {Array or Object} dictionary
+     * An array or Object containing words of interest
+     * @return {Array}            [HTML formatted words]
+     */
     this.generateWordListHTML = function(dictionary){
         var scrollable = [];
         Object.keys(dictionary).forEach(function(e){
+            var contents ;
+            if ("en" in dictionary[e]){
+                contents = `${dictionary[e]['en']} | ${e}`;
+            } else if ("ar" in dictionary[e]){
+                contents = `${e} | ${Object.values(dictionary[e]['ar']).join(", ")}`;
+            }
             scrollable.push(
-            // `<span class="tooltip">${e} ${dictionary[e]['en']}<span class="tooltiptext">${dict_index[dictionary[e]["index"]]["category"]}</span></span>`) ;
-            `<span class="tooltip"><h5>${dictionary[e]['en']} | ${e}</h5></span>`) ;
+            `<span class="word-list-entry"><h5>${contents}</h5></span><div class="word-description"></div>`) ;
         })
         return scrollable
     }
@@ -81,19 +106,19 @@ function App(){
      * @return {Array}  A list of words that have keyword in them
      */
     this.searchDictionary = function(keyword){
-        var possibleMatches = [];
+        var possibleMatches = {};
         // make a little function to search the individual dictionaries
-        var searchDictionary = function(dictionary, key){
+        var searchDictionary = function(dictionary){
             Object.keys(dictionary).forEach(function(e){
                 if (e.indexOf(keyword) !== -1){
-                    possibleMatches.push(dictionary[e][key])
+                    possibleMatches[e] = dictionary[e]
                 }
             })
         }
         // Now apply searchDictionary to the english -> Arabic and Arabic -> English
         // dictionaries:
-        searchDictionary(ar_to_en, "en");
-        searchDictionary(en_to_ar, "ar");
+        searchDictionary(ar_to_en);
+        searchDictionary(en_to_ar);
         return possibleMatches;
     }
 }
