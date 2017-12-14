963efad43324c1c26504eb6d05059d279a088846
Dean Shaff <dshaff001@gmail.com>
2017-12-14 10:27:17 +0400

    Added cloudant-api repository code

---
+++ cred.py
@@ -0,0 +1,8 @@
+arabic_ref_name = "arabic-ref"
+arabic_ref_api_key = "mselithounleseeptintomen"
+arabic_ref_api_pass = "49d47ef9450903fb3bb29f6da700afeede3ef19e"
+arabic_ref_api = {
+    'api_key':arabic_ref_api_key,
+    'api_pass':arabic_ref_api_pass,
+    'db':arabic_ref_name
+}
--- server.py
+++ server.py
@@ -1,8 +1,16 @@
+import sys
 import json
 import time
 
+sys.path.append("./cloudant-api")
+
 import requests
 from flask import Flask, render_template, jsonify, request
+
+from cred import arabic_ref_api
+from cloudant_api.api import DatabaseHandler
+
+db = DatabaseHandler(arabic_ref_api)
 
 app = Flask(
             __name__,
@@ -10,6 +18,10 @@ app = Flask(
             static_folder="client",
             static_url_path=""
 )
+
+@app.route("/get_dictionary", methods=["GET"])
+def get_dictionary():
+    return
 
 @app.route("/")
 def main():
